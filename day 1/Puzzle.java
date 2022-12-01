3dd59828df9a46738a4eaeb5cf860d8bd1a1455d
Gilles Gonzalez Oropeza <95430715+Kuuro-neko@users.noreply.github.com>
2022-12-01 11:44:14 +0000

    day 1 puzzle 2

--- day 1/Puzzle.java
+++ day 1/Puzzle.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.List;
 
-class Puzzle1 {
+class Puzzle {
     public static void main(String[] args) {
         try {
-            File file = new File("input1.txt");
+            File file = new File("input.txt");
             Scanner input = new Scanner(file);
             List<Integer> list = new ArrayList<Integer>();
             int count = 0;
@@ -28,7 +28,22 @@ class Puzzle1 {
                     max = i;
                 }
             }
-            System.out.println(max);
+            System.out.println("The elf carrying the most calories is carrying " + max + " calories."); // Puzzle 1
+
+            int[] maxes = {0, 0, 0};
+            for(int i : list) {
+                if (i > maxes[0]) {
+                    maxes[2] = maxes[1];
+                    maxes[1] = maxes[0];
+                    maxes[0] = i;
+                } else if (i > maxes[1]) {
+                    maxes[2] = maxes[1];
+                    maxes[1] = i;
+                } else if (i > maxes[2]) {
+                    maxes[2] = i;
+                }
+            }
+            System.out.println("The elves carrying the most calories are carrying " + maxes[0] + ", " + maxes[1] + ", and " + maxes[2] + " calories, which is a total of " + (maxes[0] + maxes[1] + maxes[2]) + " calories."); // Puzzle 2
         } catch (FileNotFoundException e) {
             System.out.println("File not found");
         }
