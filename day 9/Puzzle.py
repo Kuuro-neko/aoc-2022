d1ec52d3ac760f99af484afb462face447dd7057
Gilles Gonzalez Oropeza <95430715+Kuuro-neko@users.noreply.github.com>
2022-12-09 19:50:26 +0000

    small refactor

--- day 9/Puzzle.py
+++ day 9/Puzzle.py
@@ -30,19 +30,18 @@ class Rope:
             self.rope.append(knot)
     
     def move_head(self, direction):
-        # move the head
-        if direction == 'U':
-            self.rope[0].row -= 1
-        elif direction == 'R':
-            self.rope[0].column += 1
-        elif direction == 'D':
-            self.rope[0].row += 1
-        elif direction == 'L':
-            self.rope[0].column -= 1
         for knot in self.rope:
             if knot.name == 'H':
-                continue
-            if not self.is_knot_adjacent_to_its_prev(knot):
+                # move the head
+                if direction == 'U':
+                    self.rope[0].row -= 1
+                elif direction == 'R':
+                    self.rope[0].column += 1
+                elif direction == 'D':
+                    self.rope[0].row += 1
+                elif direction == 'L':
+                    self.rope[0].column -= 1
+            elif not self.is_knot_adjacent_to_its_prev(knot):
                 # move each knot diagonally towards its previous knot if it is not adjacent to it
                 if knot.column < knot.prev.column:
                     knot.column += 1
@@ -55,7 +54,7 @@ class Rope:
 
     def is_knot_adjacent_to_its_prev(self, knot):
         if knot.prev == None:
-            return False
+            raise Exception('Knot ' + knot.name + ' has no previous knot')
         return abs(knot.column - knot.prev.column) <= 1 and abs(knot.row - knot.prev.row) <= 1
     
     def get_tail_row(self):
@@ -63,16 +62,6 @@ class Rope:
     
     def get_tail_column(self):
         return self.rope[len(self.rope) - 1].column
-    
-def rotate_direction(direction):
-    if direction == 'U':
-        return 'R'
-    elif direction == 'R':
-        return 'D'
-    elif direction == 'D':
-        return 'L'
-    elif direction == 'L':
-        return 'U'
 
 def compute_aoc(rope_length, size):
     bridge = []
@@ -87,7 +76,7 @@ def compute_aoc(rope_length, size):
     for line in lines:
         motions = line.split(' ')
         for i in range(0, int(motions[1])):
-            rope.move_head(rotate_direction(motions[0]))
+            rope.move_head(motions[0])
             bridge[rope.get_tail_row()][rope.get_tail_column()] = '#'
 
     count = 0
