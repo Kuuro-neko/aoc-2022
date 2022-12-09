42532c6c608c5f5c152701180c6c73e245380af5
Gilles Gonzalez Oropeza <95430715+Kuuro-neko@users.noreply.github.com>
2022-12-09 19:13:55 +0000

    Refactoring before part 2

--- day 9/Puzzle.py
+++ day 9/Puzzle.py
@@ -2,42 +2,67 @@ file = open('input.txt', 'r')
 lines = file.readlines()
 
 class Rope:
-    def __init__(self, row, column):
-        self.head_row = row
-        self.head_column = column
-        self.tail_row = row
-        self.tail_column = column
+    class Knot:
+        def __init__(self, name, row, column):
+            self.name = name
+            self.row = row
+            self.column = column
+            self.prev = None
+            self.next = None
+        
+        def set_next(self, next):
+            self.next = next
+
+        def set_prev(self, prev):
+            self.prev = prev
+
+    def __init__(self, row, column, length):
+        self.rope = []
+        self.rope.append(self.Knot('H', row, column))
+        for n in range(1, length+1):
+            knot = self.Knot(str(n), row, column)
+            knot.set_prev(self.rope[n - 1])
+            self.rope[n - 1].set_next(knot)
+            self.rope.append(knot)
     
     def move_head(self, direction):
+        prev_knot_row = self.rope[0].row
+        prev_knot_column = self.rope[0].column
         if direction == 'U':
-            self.head_row -= 1
-        elif direction == 'D':
-            self.head_row += 1
-        elif direction == 'L':
-            self.head_column -= 1
+            self.rope[0].row -= 1
         elif direction == 'R':
-            self.head_column += 1
-        if not self.is_tail_adjacent_to_head():
-            self.move_tail_towards_head()
+            self.rope[0].column += 1
+        elif direction == 'D':
+            self.rope[0].row += 1
+        elif direction == 'L':
+            self.rope[0].column -= 1
+        for knot in self.rope:
+            if knot.name == 'H':
+                continue
+            if not self.is_knot_adjacent_to_its_prev(knot):
+                current_prev_knot_row = knot.row
+                current_prev_knot_column = knot.column
+                knot.row = prev_knot_row
+                knot.column = prev_knot_column
+                prev_knot_row = current_prev_knot_row
+                prev_knot_column = current_prev_knot_column
 
-    def move_tail_towards_head(self):
-        if self.head_row > self.tail_row:
-            self.tail_row += 1
-        elif self.head_row < self.tail_row:
-            self.tail_row -= 1
-        if self.head_column > self.tail_column:
-            self.tail_column += 1
-        elif self.head_column < self.tail_column:
-            self.tail_column -= 1
-    
-    def is_tail_adjacent_to_head(self):
-        return (abs(self.head_row - self.tail_row) <= 1 and abs(self.head_column - self.tail_column) <= 1)
+    def is_knot_adjacent_to_its_prev(self, knot):
+        if knot.prev == None:
+            return False
+        return abs(knot.column - knot.prev.column) <= 1 and abs(knot.row - knot.prev.row) <= 1
 
-    def get_head(self):
-        return (self.head_row, self.head_column)
+    def get_head_row(self):
+        return self.rope[0].row
     
-    def get_tail(self):
-        return (self.tail_row, self.tail_column)
+    def get_head_column(self):
+        return self.rope[0].column
+    
+    def get_tail_row(self):
+        return self.rope[len(self.rope) - 1].row
+    
+    def get_tail_column(self):
+        return self.rope[len(self.rope) - 1].column
     
 def rotate_direction(direction):
     if direction == 'U':
@@ -49,7 +74,7 @@ def rotate_direction(direction):
     elif direction == 'L':
         return 'U'
 
-rope = Rope(0, 0)
+rope = Rope(0, 0, 1)
 bridge = []
 size = 1000
 
@@ -63,8 +88,7 @@ for line in lines:
     motions = line.split(' ')
     for i in range(0, int(motions[1])):
         rope.move_head(rotate_direction(motions[0]))
-        bridge[rope.tail_row][rope.tail_column] = '#'
-
+        bridge[rope.get_tail_row()][rope.get_tail_column()] = '#'
 count = 0
 for row in bridge:
     for column in row:
